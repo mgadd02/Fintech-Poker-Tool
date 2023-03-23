5df84f40df73e72fcadd96d41314e14ba5146d99
Max Gadd <109029050+mgadd02@users.noreply.github.com>
2023-03-23 10:53:21 +1000

    honesly btuhb
    bruh

--- main.py
+++ main.py
@@ -1,51 +1,33 @@
-#run
-import csv
-import random
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 
-# Read in the player results from the input CSV file
-with open('player_results.csv', 'r') as f:
-    reader = csv.reader(f)
-    player_results = list(reader)
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
 
-# Convert the player results to a dictionary where the keys are the player names
-# and the values are their worth in dollars
-player_dict = {player: int(worth) for player, worth in player_results}
+        # Create labels for the outputs
+        self.first_output = QLabel(self)
+        self.first_output.move(20, 20)
+        self.second_output = QLabel(self)
+        self.second_output.move(20, 50)
+        self.third_output = QLabel(self)
+        self.third_output.move(20, 80)
 
-# Define a function to draw a winner from the remaining players
-def draw_winner(remaining_players):
-    # Calculate the total worth of the remaining players
-    total_worth = sum(player_dict[player] for player in remaining_players)
+        # Create the "Run" button
+        self.run_button = QPushButton("Run", self)
+        self.run_button.move(20, 120)
+        self.run_button.clicked.connect(self.run)
 
-    # Create a list of tuples where each tuple contains a player name and their
-    # corresponding probability of winning
-    player_probs = [(player, player_dict[player] / total_worth) for player in remaining_players]
+    def run(self):
+        # This is the function that gets called when the "Run" button is clicked
+        # Replace the code below with whatever code you want to run when the button is clicked
+        self.first_output.setText("First output")
+        self.second_output.setText("Second output")
+        self.third_output.setText("Third output")
 
-    # Draw a winner using the weighted probabilities
-    winner = random.choices([player for player, prob in player_probs], weights=[prob for player, prob in player_probs])[0]
-
-    return winner
-
-# Draw the first winner
-remaining_players = list(player_dict.keys())
-first_winner = draw_winner(remaining_players)
-
-# Remove the first winner from the list of remaining players
-remaining_players.remove(first_winner)
-
-# Draw the second winner
-second_winner = draw_winner(remaining_players)
-
-# Remove the second winner from the list of remaining players
-remaining_players.remove(second_winner)
-
-# Draw the third winner (if there are enough players remaining)
-if remaining_players:
-    third_winner = draw_winner(remaining_players)
-else:
-    third_winner = None
-
-# Write the results to the output CSV file
-with open('draw_results.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['first', 'second', 'third'])
-    writer.writerow([first_winner, second_winner, third_winner])
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    my_app = MyApp()
+    my_app.show()
+    sys.exit(app.exec())
